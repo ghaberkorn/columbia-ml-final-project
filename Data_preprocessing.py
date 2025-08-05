427353dfe24f4a3998d7ba643a69f07119d4d05b
lilpat0 <113071231+lilpat0@users.noreply.github.com>
2025-08-05 12:14:12 -0400

    Data_preprocessing.py
    Aryan .V

--- Data_preprocessing.py
+++ Data_preprocessing.py
@@ -4,55 +4,62 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import kagglehub
 
-# Download latest version
-path = kagglehub.dataset_download("emineyetm/fake-news-detection-datasets")
+def get_data():
+    """
+    Call def to get the x and y train, test data. No arguments needed
+    """
+    # Download latest version
+    path = kagglehub.dataset_download("emineyetm/fake-news-detection-datasets")
 
-print("Path to dataset files:", path)
+    print("Path to dataset files:", path)
 
-trueDF = pd.read_csv('/Users/lazytreebranch/.cache/kagglehub/datasets/emineyetm/fake-news-detection-datasets/versions/1/News _dataset/True.csv')
-falseDF = pd.read_csv('/Users/lazytreebranch/.cache/kagglehub/datasets/emineyetm/fake-news-detection-datasets/versions/1/News _dataset/Fake.csv')
+    trueDF = pd.read_csv('/Users/lazytreebranch/.cache/kagglehub/datasets/emineyetm/fake-news-detection-datasets/versions/1/News _dataset/True.csv')
+    falseDF = pd.read_csv('/Users/lazytreebranch/.cache/kagglehub/datasets/emineyetm/fake-news-detection-datasets/versions/1/News _dataset/Fake.csv')
 
-print("Length of both datasets:", len(trueDF) + len(falseDF))
+    print("Length of both datasets:", len(trueDF) + len(falseDF))
 
-# Drop NAN values, and 'date' column
-cleaned_trueDF = trueDF.dropna()
-cleaned_trueDF = cleaned_trueDF.drop(columns = ['date'])
+    # Drop NAN values, and 'date' column
+    cleaned_trueDF = trueDF.dropna()
+    cleaned_trueDF = cleaned_trueDF.drop(columns = ['date'])
 
-cleaned_falseDF = falseDF.dropna()
-cleaned_falseDF = cleaned_falseDF.drop(columns = ['date'])
+    cleaned_falseDF = falseDF.dropna()
+    cleaned_falseDF = cleaned_falseDF.drop(columns = ['date'])
 
-# Make values lower case
-cleaned_trueDF['title'] = cleaned_trueDF['title'].str.lower()
-cleaned_trueDF['text'] = cleaned_trueDF['text'].str.lower()
-cleaned_trueDF['subject'] = cleaned_trueDF['subject'].str.lower()
+    # Make values lower case
+    cleaned_trueDF['title'] = cleaned_trueDF['title'].str.lower()
+    cleaned_trueDF['text'] = cleaned_trueDF['text'].str.lower()
+    cleaned_trueDF['subject'] = cleaned_trueDF['subject'].str.lower()
 
-cleaned_falseDF['title'] = cleaned_falseDF['title'].str.lower()
-cleaned_falseDF['text'] = cleaned_falseDF['text'].str.lower()
-cleaned_falseDF['subject'] = cleaned_falseDF['subject'].str.lower()
+    cleaned_falseDF['title'] = cleaned_falseDF['title'].str.lower()
+    cleaned_falseDF['text'] = cleaned_falseDF['text'].str.lower()
+    cleaned_falseDF['subject'] = cleaned_falseDF['subject'].str.lower()
 
-# Filter out filler words, 'the', 'a', 'an', 'and', 'or', 'but'
-filler_words = ['the', 'a', 'an']
+    # Filter out filler words, 'the', 'a', 'an', 'and', 'or', 'but'
+    filler_words = ['the', 'a', 'an']
 
-def remove_filler(text):
-    return ' '.join([word for word in text.split() if word.lower() not in filler_words])
+    def remove_filler(text):
+        return ' '.join([word for word in text.split() if word.lower() not in filler_words])
 
-cleaned_trueDF['text'] = cleaned_trueDF['text'].apply(remove_filler)
-cleaned_falseDF['text'] = cleaned_falseDF['text'].apply(remove_filler)
+    cleaned_trueDF['text'] = cleaned_trueDF['text'].apply(remove_filler)
+    cleaned_falseDF['text'] = cleaned_falseDF['text'].apply(remove_filler)
 
 
-# split into features and labels    
-X_true = cleaned_trueDF
-y_true = np.ones(len(X_true), dtype=bool)    # creates an arry of true
+    # split into features and labels    
+    X_true = cleaned_trueDF
+    y_true = np.ones(len(X_true), dtype=bool)    # creates an arry of true
 
-X_false = cleaned_falseDF
-y_false = np.zeros(len(X_false), dtype=bool) # creates an array of false
+    X_false = cleaned_falseDF
+    y_false = np.zeros(len(X_false), dtype=bool) # creates an array of false
 
-# concatenate X and y
-X = pd.concat([X_true, X_false], ignore_index=True)
-y = np.concatenate([y_true, y_false])
+    # concatenate X and y
+    X = pd.concat([X_true, X_false], ignore_index=True)
+    y = np.concatenate([y_true, y_false])
 
-# split into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # split into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # return data
+    return X_train, X_test, y_train, y_test
 
 
 
