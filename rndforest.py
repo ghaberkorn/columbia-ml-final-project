fe93dba4c1fa681ae5d783ac4c3a2f740b1c3f30
Atter Studios <147772193+ghaberkorn@users.noreply.github.com>
2025-08-05 09:46:56 -0400

    f

--- rndforest.py
+++ rndforest.py
@@ -1,1 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 # random forest classifier code
